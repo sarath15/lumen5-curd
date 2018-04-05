ae67c4414f3c1fb980829a5a817eababd5db947e
sarathkumarV_Contus <sarathkumar.v@contus.in>
2018-04-05 11:06:22 +0530

    Added middleware for handling CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,9 +59,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+   App\Http\Middleware\CorsMiddleware::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
@@ -82,6 +82,8 @@ $app->singleton(
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
+ 
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
