55dfcba23bcdff13ce11401ba162fcd39372a781
sarathkumarV_Contus <sarathkumar.v@contus.in>
2018-04-18 18:18:38 +0530

    added the options in cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,7 +3,7 @@ class CorsMiddleware {
   public function handle($request, \Closure $next)
   {
     $response = $next($request);
-    $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
+    $response->header('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
     $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
     $response->header('Access-Control-Allow-Origin', '*');
     return $response;
